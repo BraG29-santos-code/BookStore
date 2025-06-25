2024dee3aaf2748cb04925d0909065beeaf406f8
BraG29-santos-code <bgranero@santoscode.com>
2025-06-25 16:32:19 -0300

    Initial Commit - Completado hasta la parte 4 del tutorial de Book Store Application

---
+++ test/Acme.BookStore.EntityFrameworkCore.Tests/Books/BookAppService_Tests.cs
@@ -0,0 +1,10 @@
+﻿using Acme.BookStore.EntityFrameworkCore;
+using Xunit;
+
+namespace Acme.BookStore.Books;
+
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class BookAppService_Tests : BookAppService_Tests<BookStoreEntityFrameworkCoreTestModule>
+{
+    
+}
