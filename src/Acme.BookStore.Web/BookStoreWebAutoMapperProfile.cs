7d7b9ea5b3aece5d391647bc1109cbabeae91e25
BraG29-santos-code <bgranero@santoscode.com>
2025-06-26 17:19:33 -0300

    Tutorial Book Store Application: Parte 9 (Authors: User Interface)

--- src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -1,5 +1,7 @@
+using Acme.BookStore.Authors;
 using Acme.BookStore.Books;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Acme.BookStore.Web;
 
@@ -8,6 +10,14 @@ public class BookStoreWebAutoMapperProfile : Profile
     public BookStoreWebAutoMapperProfile()
     {
         //Define your object mappings here, for the Web project
+        
+        //Libros
         CreateMap<BookDto, CreateUpdateBookDto>();
+        
+        //Autores
+        CreateMap<Pages.Authors.CreateModalModel.CreateAuthorViewModel, CreateAuthorDto>();
+        CreateMap<AuthorDto, Pages.Authors.EditModalModel.EditAuthorViewModel>();
+        CreateMap<Pages.Authors.EditModalModel.EditAuthorViewModel,
+            UpdateAuthorDto>();
     }
 }
---
+++ src/Acme.BookStore.Web/Pages/Authors/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Acme.BookStore.Web.Pages.Authors;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
